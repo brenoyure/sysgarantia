c236048871c903e9b0802b32000cdd84f2b60ca6
Breno Yuri <byurifb@gmail.com>
2024-12-03 20:13:08 -0300

    Revert "xd"
    This reverts commit 084ccfffdc2821d9d64de22a53655fb4f2fd3f22.

--- api/src/main/java/br/albatross/sysgarantia/domain/models/cliente/ClienteComboBox.java
+++
@@ -1,8 +0,0 @@
-package br.albatross.sysgarantia.domain.models.cliente;
-
-public record ClienteComboBox(
-        int id, 
-        String nome, 
-        String descricao) 
-    {
-}
--- api/src/main/java/br/albatross/sysgarantia/persistence/repositories/problemas/DescricaoProblemaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.albatross.sysgarantia.persistence.repositories.problemas;
-
-import br.albatross.sysgarantia.persistence.entities.problemas.DescricaoProblema;
-import br.albatross.sysgarantia.persistence.repositories.Repository;
-
-public interface DescricaoProblemaRepository extends Repository<DescricaoProblema, Integer> {
-
-}
