402b095972a68681ff9580c9a2f621d748d1b61a
Breno Yuri <byurifb@gmail.com>
2025-01-20 09:15:44 -0300

    imports incorretos removidos do MessageProducer

--- api/src/main/java/br/albatross/sysgarantia/messaging/MessagesProducerService.java
+++ api/src/main/java/br/albatross/sysgarantia/messaging/MessagesProducerService.java
@@ -6,11 +6,13 @@ import org.eclipse.microprofile.reactive.messaging.Message;
 
 import br.albatross.sysgarantia.models.Email;
 import br.albatross.sysgarantia.models.SolicitacaoGarantia;
-import br.albatross.sysgarantia.repositories.EmailRepository;
-import br.albatross.sysgarantia.services.garantia.QuarkusMailerService;
+
 import jakarta.enterprise.context.ApplicationScoped;
+
 import jakarta.inject.Inject;
+
 import jakarta.json.Json;
+
 import jakarta.transaction.Transactional;
 import jakarta.transaction.Transactional.TxType;
 
@@ -20,12 +22,6 @@ public class MessagesProducerService {
     @Inject
     @Channel("out-sysgarantia-novas-solicitacoes-channel")
     Emitter<String> novasSolicitacoesMessageProducer;
-
-    @Inject
-    EmailRepository emailRepository;    
-
-    @Inject
-    QuarkusMailerService emailService; 
 
     @Transactional(TxType.NOT_SUPPORTED)
     public void send(Email emailGarantia) {
