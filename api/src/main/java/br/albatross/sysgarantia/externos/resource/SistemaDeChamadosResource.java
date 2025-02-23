ea557f5623d368091310c14f36799f68bf929a90
EC2 Default User <ec2-user@ip-172-31-10-15.us-east-2.compute.internal>
2025-02-23 20:05:21 +0000

    Tentando corrigir o kafka

--- api/src/main/java/br/albatross/sysgarantia/externos/resource/SistemaDeChamadosResource.java
+++ api/src/main/java/br/albatross/sysgarantia/externos/resource/SistemaDeChamadosResource.java
@@ -1,8 +1,10 @@
 package br.albatross.sysgarantia.externos.resource;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import br.albatross.sysgarantia.externos.SistemaDeChamados;
+import br.albatross.sysgarantia.externos.dto.*;
 
 import jakarta.inject.Inject;
 
@@ -30,12 +32,26 @@ public class SistemaDeChamadosResource {
     @POST
     @Path("/chamados")
     public Response listarChamadosPelosServicosDoFornecedor(@NotEmpty List<Integer> ids) {
+        // List<Chamado> chamados = new ArrayList<>();
+        // chamados.add( new Chamado(486L, "2025010682000486", "Monitor com Tela Piscando", 100, "Monitor DATEN", "usuario@example.com") );
+        // chamados.add( new Chamado(487L, "2025010682000487", "PC Não Liga", 100, "Gabinete DATEN", "usuario@example.com") );
+        // chamados.add( new Chamado(488L, "2025010682000488", "Tecla ENTER sem funcionar", 100, "Teclado DATEN", "usuario@example.com") );
+        // chamados.add( new Chamado(489L, "2025010682000489", "Mouse com clique duplo", 100, "Mouse DATEN", "usuario@example.com") );
+
+        // return Response.ok(chamados).build();
         return Response.ok(sistemaDeChamados.findByService(ids)).build();
     }
 
     @GET
     @Path("/chamados/servicos")
     public Response listarServicosDosChamados() {
+        // List<Servico> servicos = new ArrayList<>();
+        // servicos.add(new Servico(98, "Garantia DATEN::Gabinete DATEN"));
+        // servicos.add(new Servico(98, "Garantia DATEN::Mouse DATEN"));
+        // servicos.add(new Servico(98, "Garantia DATEN::Monitor DATEN"));
+        // servicos.add(new Servico(98, "Garantia DATEN::Teclado DATEN"));
+
+        // return Response.ok(servicos).build();
         return Response.ok(sistemaDeChamados.findAllTicketsServices()).build();
     }
 
@@ -43,10 +59,14 @@ public class SistemaDeChamadosResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/inventario/{identificador}")
     public Response buscarNumeroDeSeriePeloIdentificador(@NotBlank @PathParam("identificador") String identificador) {
-        return sistemaDeChamados
-                .findSerialNumberByIdentifier(identificador)
-                .map(numeroDeSerie -> Response.ok(numeroDeSerie).build())
-                .orElse(Response.status(Status.NOT_FOUND).build());
+        // if (identificador.equals("204703")) {
+        //     return Response.ok("AVCLX486").build();
+        // }
+        // return Response.status(Status.NOT_FOUND).build();
+           return sistemaDeChamados
+                    .findSerialNumberByIdentifier(identificador)
+                    .map(numeroDeSerie -> Response.ok(numeroDeSerie).build())
+                    .orElse(Response.status(Status.NOT_FOUND).build());
     }
 
 }
